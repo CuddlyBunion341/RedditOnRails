358d8fe5261bccfcd98eb39c144f904168e94e16
CuddlyBunion341 <daniel@bengl.cz>
2022-12-05 14:25:58 +0100

    user model validation, sass hotfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, presence: true, uniqueness: true, format: { with: /[a-zA-Z0-9_]{1,20}/, message: "Invalid Username" }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid Email" }
 end
