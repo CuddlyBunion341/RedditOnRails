fd4913ec5f6861d207acdf0e143840538be6577e
CuddlyBunion341 <daniel@bengl.cz>
2022-12-08 21:35:27 +0100

    created a basic registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "post#index"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
+  get "signup", to: "registrations#new"
+  post "signup", to: "registrations#create"
 end
