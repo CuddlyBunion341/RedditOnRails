40f7492231d93742c05ca8fafc8c114c9dda5b79
CuddlyBunion341 <daniel@bengl.cz>
2022-12-20 13:07:33 +0100

    added a navigation to user page + css refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   root "posts#index"
-  get "login", to: "sessions#new"
-  post "login", to: "sessions#create"
-  get "signup", to: "registrations#new"
-  post "signup", to: "registrations#create"
-  delete "logout", to: "sessions#destroy"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/signup", to: "registrations#new"
+  post "/signup", to: "registrations#create"
+  delete "/logout", to: "sessions#destroy", as: "logout"
 
-  get "create_post", to: "posts#new"
-  post "create_post", to: "posts#create"
+  get "/create_post", to: "posts#new"
+  post "/create_post", to: "posts#create"
 
-  match "upvote_post/:id", to: "posts#upvote", via: [:get, :post], as: "upvote_post"
-  match "downvote_post/:id", to: "posts#downvote", via: [:get, :post], as: "downvote_post"
+  match "/upvote_post/:id", to: "posts#upvote", via: [:get, :post], as: "upvote_post"
+  match "/downvote_post/:id", to: "posts#downvote", via: [:get, :post], as: "downvote_post"
 
-  match "save_post/:id", to: "posts#save", via: [:get, :post], as: "save_post"
+  match "/save_post/:id", to: "posts#save", via: [:get, :post], as: "save_post"
 
-  match "archive_post/:id", to: "posts#archive", via: [:get, :post], as: "archive_post"
+  match "/archive_post/:id", to: "posts#archive", via: [:get, :post], as: "archive_post"
 
   resources :users, only: [:show, :index]
 
