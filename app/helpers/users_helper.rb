8f1150766436677809a4025807e8be07211a865f
CuddlyBunion341 <daniel@bengl.cz>
2022-12-21 10:31:15 +0100

    refactored compact post variant & improved post styling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,12 @@ module UsersHelper
   def user_tab_link(link_text, user, tab = link_text.downcase)
     link_path = user_path user.username, tab: tab
 
-    class_name = current_page?(link_path) ? "active" : ""
+    class_name = "active" if current_page?(link_path)
+
+    if params[:tab].blank? && tab == "overview"
+      class_name = "active"
+    end
+
     content_tag(:li, :class => class_name) do
       link_to link_text, link_path
     end
