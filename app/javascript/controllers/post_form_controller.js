a704589dca245d4f60bbb6d54b47da8168f0446f
CuddlyBunion341 <daniel@bengl.cz>
2022-12-29 19:49:39 +0100

    changed post composer styling

--- app/javascript/controllers/post_form_controller.js
+++ app/javascript/controllers/post_form_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-	static targets = ["wrapper", "tabs"];
+	static targets = ["wrapper", "tabs", "uploadList"];
 
 	connect() {}
 
@@ -15,5 +15,23 @@ export default class extends Controller {
 		this.wrapperTarget
 			.querySelector(`#tab${tab}`)
 			.classList.remove("hidden");
+	}
+
+	upload(event) {
+		const files = event.target.files;
+
+		for (let i = 0; i < files.length; i++) {
+			let file = files.item(i);
+
+			const reader = new FileReader();
+
+			reader.onload = (e) => {
+				const image = document.createElement("img");
+				image.src = e.target.result;
+				this.uploadListTarget.appendChild(image);
+			};
+
+			reader.readAsDataURL(file);
+		}
 	}
 }
