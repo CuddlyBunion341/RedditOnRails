692dab6e95bfdd8c5ca50fbc85db86882dde1e82
CuddlyBunion341 <daniel@bengl.cz>
2023-01-07 23:29:32 +0100

    added post sorting to communities

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -5,5 +5,10 @@ class CommunitiesController < ApplicationController
 
   def show
     @community = Community.find_by(shortname: params[:name])
+    if params[:sort] == "top"
+      @posts = @community.posts.where(status: "public").order(score: :desc)
+    else
+      @posts = @community.posts.where(status: "public").order(created_at: :desc)
+    end
   end
 end
